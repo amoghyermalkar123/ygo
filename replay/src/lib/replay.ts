30af8dd298f7e1cf8dfa44e8769f4be7743c196b
ayermalkar123 <ayermalkar99@gmail.com>
2025-04-18 14:13:34 +0530

    asd

--- replay/src/lib/replay.ts
+++
@@ -1,6 +0,0 @@
-import type { Event } from './types'
-import events from '../../events.json'
-
-export function getEvents(): Event[] {
-  return events
-}
--- replay/src/lib/store.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store'
-import { getEvents } from './replay'
-
-const events = getEvents()
-export const currentIndex = writable(0)
-export const currentEvent = writable(events[0])
-export const allEvents = writable(events)
-
-currentIndex.subscribe(i => currentEvent.set(events[i]))
